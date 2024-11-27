05163e5e25c4f89ae49dbd3d0d22d19110489ed8
irfanyazid-13032000 <irfanyazid18@gmail.com>
2024-11-27 12:23:03 +0700

    membuat perubahan lagi

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\TambahtambahanController;
 Route::get('/soal', [KanjiController::class, 'soal']);
 
 Route::get('/perubahan', function(){
-  return 'tes perubahan';
+  return 'tes perubahan lagi';
 });
 
 Route::post('/insertemailtambahtambahan', [TambahtambahanController::class, 'store']);
